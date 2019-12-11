940952e1acb0ad1eb2d62c940a1e32dd2a4f56b6
Aurélien Roux <aurelien.roux@aerial.ai>
2019-12-10 21:31:41 -0500

    create useEffect hooks

--- src/Components/SongList.js
+++ src/Components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import uuid from 'uuid/v1'
 import { NewSongForm } from './NewSongForm'
 
@@ -8,10 +8,19 @@ export const SongList = () => {
     { title: 'Song 2', id: uuid() },
     { title: 'Song 3', id: uuid() }
   ])
+  const [age, setAge] = useState(20)
 
   const addSong = title => {
     setSongs([...songs, { title, id: uuid() }])
   }
+
+  useEffect(() => {
+    console.log('renders songs', songs)
+  }, [songs])
+
+  useEffect(() => {
+    console.log('renders age', age)
+  }, [age])
 
   return (
     <div className="song-list">
@@ -22,6 +31,9 @@ export const SongList = () => {
       </ul>
 
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>
+        click to add 1 to age: {age}
+      </button>
     </div>
   )
 }
